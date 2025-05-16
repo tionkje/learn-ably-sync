a6015212ecb01deb60c9c7a2a0fde53413ac8660
Bastiaan Deknudt <deknudtb@gmail.com>
2025-05-16 18:23:27 +0200

    no well-known

---
+++ src/hooks.server.ts
@@ -0,0 +1,10 @@
+import { error, type Handle } from '@sveltejs/kit';
+
+export const handle: Handle = async ({ event, resolve }) => {
+  if (event.url.pathname.endsWith('favicon.ico'))
+    error(404, 'Favicon not found');
+  if (event.url.pathname.startsWith('/.well-known/')) 
+    error(404, 'not well-known');
+
+  return await resolve(event);
+};
