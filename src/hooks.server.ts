e795bc2016b821d0fae31f86d365bfcd6adef65e
Bastiaan Deknudt <deknudtb@gmail.com>
2025-05-16 20:21:43 +0200

    updates

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,10 +1,8 @@
 import { error, type Handle } from '@sveltejs/kit';
 
 export const handle: Handle = async ({ event, resolve }) => {
-  if (event.url.pathname.endsWith('favicon.ico'))
-    error(404, 'Favicon not found');
-  if (event.url.pathname.startsWith('/.well-known/')) 
-    error(404, 'not well-known');
+  if (event.url.pathname.endsWith('favicon.ico')) error(404, 'Favicon not found');
+  if (event.url.pathname.startsWith('/.well-known/')) error(404, 'not well-known');
 
   return await resolve(event);
 };
